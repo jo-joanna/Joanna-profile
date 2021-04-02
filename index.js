0b119d0b1aa206716bb1e59e7c2f7f0ca66221d1
jo-joanna <b.joanna.uk@gmail.com>
2021-04-02 21:52:06 +0200

    JS link moved to the bottom
    To enable JS work, this had to be moved to before the end of the tag </html>

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ This will be built-in into a function to go through every image  */
       for (let i = 0; i < images.length; i++) {
         let imageElement = images[i];
 
-//condition to display image's array
+      //condition to display image's array. classList is a selector
         if (animal === "all" | imageElement.classList.contains(animal)) {
           imageElement.style.display = "block";
         } else {
@@ -24,6 +24,23 @@ This will be built-in into a function to go through every image  */
       }
     }
 
+/* Loop to select buttons and active CSS button style */
+    function updatedClickedButton(selectedButton) {
+      for (let i = 0; i < button.length; i++) {
+        if (button[i] === selectedButton) {
+          //the button selected
+          button[i].classList.add("selected");
+        } else {
+          // the button not clicked
+          button[i].classList.remove("selected")
+        }
+      }
+    }
+
+/* Helper text saying something that matches the search */
+    function updateHelperText(animal) {
+      helperText.innerHTML = "You're looking at the pictures of " + animal;
+    }
 
 /* Loop through each button in the button array. Add the listener for the loop that looks for a click event*/
 for (let i = 0; i < button.length; i++) {
@@ -32,9 +49,25 @@ for (let i = 0; i < button.length; i++) {
     let animal = button[i].dataset.animal;
     console.log(animal);
 
-// call other function in the loop to be actioned.
+   //You must call other function in the loop to be actioned.
     updateImageDisplay(animal);
-    updateClickButton(button[i]);
+    updatedClickedButton(button[i]);
     updateHelperText(animal);
   });
 };
+
+/* Search event to display what we look for */
+  searchBox.addEventListener("keydown", function(event) {
+    let mySearchTerm = this.value;
+
+    for (let i = 0; i < images.length; i++) {
+      let imageElement = images[i];
+
+      if (mySearchTerm === "all" || imageElement.getAttribute("class").includes(mySearchTerm)) {
+        //searching
+        imageElement.style.display = "block";
+      } else {
+        imageElement.style.display = "none";
+      }
+    }
+  });
