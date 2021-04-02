a341f47531f77500a65ddc3d6c31e5cb1dd07ba6
jo-joanna <b.joanna.uk@gmail.com>
2021-04-02 20:28:49 +0200

    1 missed picture

--- index.js
+++ index.js
@@ -8,6 +8,23 @@
 
     console.log(button);
 
+
+/* Loop through each item in the images array to change containers to display:block or dicplay:none
+This will be built-in into a function to go through every image  */
+    function updateImageDisplay(animal) {
+      for (let i = 0; i < images.length; i++) {
+        let imageElement = images[i];
+
+//condition to display image's array
+        if (animal === "all" | imageElement.classList.contains(animal)) {
+          imageElement.style.display = "block";
+        } else {
+          imageElement.style.display ="none";
+        }
+      }
+    }
+
+
 /* Loop through each button in the button array. Add the listener for the loop that looks for a click event*/
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("click", function(event) {
