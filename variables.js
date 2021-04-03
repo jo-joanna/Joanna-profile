1b4fa2132d1b328b764bc9341acf530adba45dc9
jo-joanna <b.joanna.uk@gmail.com>
2021-04-03 18:49:39 +0200

    Added task 2&3

--- variables.js
+++ variables.js
@@ -2,3 +2,14 @@
 
 console.log("Hi there!");
 //alert("You will not see a magic if you have turned off JavaScript");
+
+//task 2
+let numberOfKittens = 5;
+let numberOfPuppies = 8;
+let totalpupils = numberOfKittens + numberOfPuppies;
+
+console.log(totalpupils);
+console.log("Anne has got " + totalpupils + " pupils");
+console.log(`${numberOfKittens} kittens belong to Anne\'s daughter and ${numberOfPuppies} puppies belong Anne\'s son.`)
+
+//task 3
