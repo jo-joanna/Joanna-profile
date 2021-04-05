dce2755d25ff2d28e3c8a83e917d17c798c2097f
jo-joanna <b.joanna.uk@gmail.com>
2021-04-05 19:30:13 +0200

    Added task9 + loops + display the loop in html
    Used DOM methods, used loops and objects.

--- variables.js
+++ variables.js
@@ -104,3 +104,41 @@ for (var i = 0; i <myFavouriteFoods.length; i++) {
 }
 
 //Task9
+let recipe = {
+  recipeTitle: "Chocolate cake",
+  servings: 4,
+  ingredients: ["350g of flour", " 120g of sugar", " 120ml of oil", " 4tbsp of cocoa powder", " 150ml of milk", " 2 tsp of baking powder"],
+  directions: ["1. mixed all dry ingredients", " 2. mixed all wet ingredients", " 3. add wet to dry and stir", " 4. warm up oven to 180deg.", " 5. line a parchment in the baking trey", " 6. pour the dough into the baking trey", " 7. put the trey into the warm oven and leave it for an hour", " 8. take it out and cool down on the rack and then, serve."]
+
+}//this is an object
+document.getElementById("p1").innerHTML = `<b>List of properties</b> <br /> ${recipe.recipeTitle}<br />Servings: ${recipe.servings}<br />Ingredients: ${recipe.ingredients}<br />`;
+//this is element to pass object to html by using ID
+
+for (i = 0; i < recipe.directions.length; i++) {
+  directions = recipe.directions[i];
+  console.log(directions);
+}//this is a loop to list directions in console.
+
+for (i=0; i < recipe.ingredients.length; i++) {
+  console.log(`${i + 1}. ${recipe.ingredients[i]}`);
+}//this is a loop to list ingredients in console.
+
+//To pass a loop outcome of arrays, object being declared as STRING you need to declare a variable of element you'll use.
+//Then you create a loop and in it declare an access to an array/object.
+//Next you call the declared element + .innerHTML, to which you'll append directions array/object every time through the loop.
+//If you don't append you'll replace entire .innerHTML, thus you'll see the last value of an array/object.
+
+//The last is to add title you need to call declared variable element/method +innerText/Content/html.
+  let direction = document.getElementById("p2");
+  direction.innerHTML = "<b>A loop to list all the directions:</b><br>"; //alternative: direction.innerContent = "A loop to list all the directions\n" - with a new line
+for (i = 0; i < recipe.directions.length; i++) {
+  directions = recipe.directions[i];
+  direction.innerHTML += directions + "<br>";
+}
+
+  let ingredient = document.getElementById("p3");
+  ingredient.innerHTML = "<b>A loop to list all the ingredients:</b><br>";
+for (i = 0; i < recipe.ingredients.length; i++) {
+  ingredients = recipe.ingredients[i];
+  ingredient.innerHTML += "- " + ingredients + "<br>";
+}
